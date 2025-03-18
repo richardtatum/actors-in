cf37ffbb17a61d8a6c34d8dece78585214ff2192
richardtatum <rtatum@pm.me>
2025-03-18 23:05:36 +0000

    Move response struct to the file, as it differs per endpoint. Pass api_client as a ref

--- src/api/actors.rs
+++ src/api/actors.rs
@@ -1,13 +1,9 @@
 use super::client::ApiClient;
-use serde::Deserialize;
-
-pub struct ActorService {
-    api_client: ApiClient,
-}
+use serde::{Deserialize, de::DeserializeOwned};
 
 #[derive(Deserialize, Debug)]
 pub struct SearchPersonResult {
-    pub id: u32,
+    pub id: i32,
     pub name: String,
     pub known_for_department: String,
     pub known_for: Vec<KnownFor>,
@@ -19,14 +15,45 @@ pub struct KnownFor {
     pub title: Option<String>, // It's 'title' for Movies, 'name' for TV
 }
 
-impl ActorService {
-    pub fn new(api_client: ApiClient) -> Self {
+#[derive(Deserialize, Debug)]
+pub struct GetCreditsResult {
+    pub cast: Vec<KnownFor>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(bound = "T: DeserializeOwned")]
+struct Response<T: DeserializeOwned> {
+    page: u32,
+    results: Vec<T>,
+}
+
+pub struct ActorService<'a> {
+    api_client: &'a ApiClient,
+}
+
+impl<'a> ActorService<'a> {
+    pub fn new(api_client: &'a ApiClient) -> Self {
         Self { api_client }
     }
 
     pub async fn search_actor(&self, s: &str) -> Result<Vec<SearchPersonResult>, reqwest::Error> {
         // Will need to add pagination support
         let endpoint = format!("3/search/person?query={}", s);
-        return self.api_client.get::<SearchPersonResult>(&endpoint).await;
+        let response = self
+            .api_client
+            .get::<Response<SearchPersonResult>>(&endpoint)
+            .await?;
+
+        return Ok(response.results);
+    }
+
+    pub async fn get_credits(&self, id: &u32) -> Result<Vec<GetCreditsResult>, reqwest::Error> {
+        let endpoint = format!("3/person/{id}/movie_credits");
+        let response = self
+            .api_client
+            .get::<Response<GetCreditsResult>>(&endpoint)
+            .await?;
+
+        return Ok(response.results);
     }
 }
