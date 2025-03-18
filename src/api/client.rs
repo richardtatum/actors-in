d165257ef55a81613b9e4df206f4d5d722e1c311
richardtatum <rtatum@pm.me>
2025-03-18 23:05:56 +0000

    Accept the entire inbound generic, instead of wrapping it around Response<T>

--- src/api/client.rs
+++ src/api/client.rs
@@ -18,7 +18,7 @@ impl ApiClient {
     }
 
     // Update this to return T, when I work out how to define T is deserialisable
-    pub async fn get<T: DeserializeOwned>(&self, endpoint: &str) -> Result<Vec<T>, reqwest::Error> {
+    pub async fn get<T: DeserializeOwned>(&self, endpoint: &str) -> Result<T, reqwest::Error> {
         let url = format!("{}/{}", self.base_url, endpoint);
         let response = self
             .client
@@ -26,16 +26,9 @@ impl ApiClient {
             .bearer_auth(&self.api_key)
             .send()
             .await?
-            .json::<Response<T>>()
+            .json::<T>()
             .await?;
 
-        Ok(response.results)
+        Ok(response)
     }
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(bound = "T: DeserializeOwned")]
-struct Response<T: DeserializeOwned> {
-    page: u32,
-    results: Vec<T>,
 }
